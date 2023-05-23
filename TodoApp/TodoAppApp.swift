229dfbd1a1e96de40f0d0f96272217f75e07a7e9
Dmitro Dohryk <dmitro.dohryk@gmail.com>
2023-05-23 22:36:16 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Dmitro Dohryk on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
